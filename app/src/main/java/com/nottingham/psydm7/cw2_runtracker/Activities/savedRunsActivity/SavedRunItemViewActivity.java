250f57c7533c5af20d0bc53841899a97d41f1bc3
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2020-12-12 14:15:55 +0000

    Created new empty files for viewing the saved runs

---
+++ app/src/main/java/com/nottingham/psydm7/cw2_runtracker/Activities/savedRunsActivity/SavedRunItemViewActivity.java
@@ -0,0 +1,16 @@
+package com.nottingham.psydm7.cw2_runtracker.Activities.savedRunsActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nottingham.psydm7.cw2_runtracker.R;
+
+public class SavedRunItemViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_run_item_view);
+    }
+}
---
+++ app/src/main/java/com/nottingham/psydm7/cw2_runtracker/Activities/savedRunsActivity/SavedRunsRecyclerViewAdapter.java
@@ -0,0 +1,4 @@
+package com.nottingham.psydm7.cw2_runtracker.Activities.savedRunsActivity;
+
+public class SavedRunsRecyclerViewAdapter {
+}
